272d087ccf0f550856f065d230e5a280c9b70846
Kiril Vatev <vatev.1@gmail.com>
2020-07-27 18:19:35 -0400

    adding stubbed tests and linting

---
+++ test/maker.test.js
@@ -0,0 +1,13 @@
+describe('app-icon-maker', () => {
+  it('generates all icons by default');
+
+  it('can optionally skip icns output');
+
+  it('can optionally skip ico output');
+
+  it('can optionally skip the png output');
+
+  it('can optionally skip the svg output');
+
+  it('can optionally generate arbitrary png sizes');
+});
