47fce44920bc720069a69efdb0dd9446b98d5b60
Kiril Vatev <vatev.1@gmail.com>
2023-12-14 00:25:10 -0500

    cleanup so more tests can be added

--- test/generator.test.js
+++ test/generator.test.js
@@ -4,19 +4,28 @@ const { generateIcons } = require('../');
 const { validateIcons, svg } = require('./helpers');
 
 describe('app-icon-maker API generateIcons', () => {
-  it('generates all icons by default usign svg string', async () => {
+  const validateIconEntity = icon => {
+    expect(icon).to.have.all.keys(['buffer', 'name', 'ext']);
+    expect(icon.buffer).to.be.instanceOf(Buffer);
+    expect(icon.ext).to.be.a('string');
+    expect(icon.name).to.be.a('string');
+
+    expect(icon.name.split('.').pop()).to.equal(icon.ext);
+  };
+
+  const getGeneratedIcons = async generator => {
     const icons = {};
 
-    for await (const icon of generateIcons(svg)) {
-      expect(icon).to.have.all.keys(['buffer', 'name', 'ext']);
-      expect(icon.buffer).to.be.instanceOf(Buffer);
-      expect(icon.ext).to.be.a('string');
-      expect(icon.name).to.be.a('string');
-
-      expect(icon.name.split('.').pop()).to.equal(icon.ext);
-
+    for await (const icon of generator) {
+      validateIconEntity(icon);
       icons[icon.name] = icon.buffer;
     }
+
+    return icons;
+  };
+
+  it('generates all icons by default usign svg string', async () => {
+    const icons = await getGeneratedIcons(generateIcons(svg));
 
     expect(Object.keys(icons)).to.have.a.lengthOf(6);
 
